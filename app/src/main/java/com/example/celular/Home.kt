a80dc7c48ab9156dfb4a1688ee8e43029c37a555
lucasantonio12 <lucas-antonio2011@hotmail.com>
2019-10-02 15:33:06 -0300

    frist

---
+++ app/src/main/java/com/example/celular/Home.kt
@@ -0,0 +1,14 @@
+package com.example.celular
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Home : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/celular/RegisterBooks.kt
@@ -0,0 +1,12 @@
+package com.example.celular
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterBooks : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_books)
+    }
+}
